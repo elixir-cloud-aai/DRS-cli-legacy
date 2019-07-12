f043d945cbb41fa3589dd70d4ff65e47e9e05f06
vaniisgh <vanisingh@live.co.uk>
2019-07-12 07:34:34 +0400

    changes to allow installation
    - add a setup and manifest file
    - change folder structure to mirror that of TES-cli to ensure uniformity

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import (setup, find_packages)
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='DRS_cli',
+    version='0.1.0',
+    author='ELIXIR Europe',
+    author_email='vani11537@one.ducic.ac.in',
+    description='bravado generated mock GA4GH DRS client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache License 2.0',
+    url='https://github.com/elixir-europe/DRS-cli',
+    packages=find_packages(),
+    keywords=(
+        'ga4gh tes elixir client restful api app openapi '
+        'swagger python bravado'
+    ),
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=['bravado[fido]'],
+    python_requires='>=3'
+)
